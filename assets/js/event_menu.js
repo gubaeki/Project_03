618e32b0dd9466f7b3d3d54a478485ea885bed9c
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-07-19 16:18:38 +0900

    Update event_menu.js

--- assets/js/event_menu.js
+++ assets/js/event_menu.js
@@ -14,10 +14,11 @@ document.addEventListener('click', function(event) {
         var gifRect = moving_tiger.getBoundingClientRect();
 
         // 뷰포트 크기 계산
-        var viewportHeight = window.innerHeight;
-        var viewportWidth = window.innerWidth;
+        //var viewportHeight = window.innerHeight;
+        //var viewportWidth = window.innerWidth;
         //var kboat_minX = 56.3 * viewportWidth / 100, kboat_maxX = 97.6 * viewportWidth / 100;
-        message.textContent = `현재 좌표: (${viewportWidth}, ${viewportHeight}`;
+        //message.textContent = `현재 좌표: (${viewportWidth}, ${viewportHeight}`;
+        message.textContent = `나오나?`;
         message.style.display = 'block';
         /*
         // 특정 범위 정의 (예: x 100-200, y 100-200)
