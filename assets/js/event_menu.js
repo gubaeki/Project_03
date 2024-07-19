c2f8e6c073d2617272814afa1ff6f55887fe82a3
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-07-19 15:54:29 +0900

    Update event_menu.js

--- assets/js/event_menu.js
+++ assets/js/event_menu.js
@@ -18,28 +18,30 @@ document.addEventListener('click', function(event) {
         var viewportWidth = window.innerWidth;
 
         // 특정 범위 정의 (예: x 100-200, y 100-200)
-        var minX = 100, maxX = 2000, minY = 100, maxY = 2000;
-        var kboat_minX = 56.3 * viewportWidth / 100, kboat_maxX = 97.6 * viewportWidth, kboat_minY = 18.4 * viewportHeight / 100, kboat_maxY = 35.5 * viewportHeight / 100;
-        var krace_minX = 10 * viewportWidth / 100, krace_maxX = 37.3 * viewportWidth, krace_minY = 45 * viewportHeight / 100, krace_maxY = 62.7 * viewportHeight / 100;
-        var krun_minX = 61.6 * viewportWidth / 100, krun_maxX = 96 * viewportWidth, krun_minY = 75.9 * viewportHeight / 100, krun_maxY = 89 * viewportHeight / 100;
+        var kboat_minX = 56.3 * viewportWidth / 100, kboat_maxX = 97.6 * viewportWidth / 100;
+        var kboat_minY = 18.4 * viewportHeight / 100, kboat_maxY = 35.5 * viewportHeight / 100;
+        var krace_minX = 10 * viewportWidth / 100, krace_maxX = 37.3 * viewportWidth / 100;
+        var krace_minY = 45 * viewportHeight / 100, krace_maxY = 62.7 * viewportHeight / 100;
+        var krun_minX = 61.6 * viewportWidth / 100, krun_maxX = 96 * viewportWidth / 100;
+        var krun_minY = 75.9 * viewportHeight / 100, krun_maxY = 89 * viewportHeight / 100;
 
+        var message = document.getElementById('event_sub');
         
         // 범위 안에 있는지 검사
         if (gifRect.left >= kboat_minX && gifRect.right <= kboat_maxX && gifRect.top >= kboat_minY && gifRect.bottom <= kboat_maxY) {
-            var message = document.getElementById('event_sub');
             message.textContent = `경정! 현재 좌표: (${gifRect.left.toFixed(2)}, ${gifRect.top.toFixed(2)})`;
             message.style.display = 'block';
-        }
-        else if(gifRect.left >= krace_minX && gifRect.right <= krace_maxX && gifRect.top >= krace_minY && gifRect.bottom <= krace_maxY) {
-            var message = document.getElementById('event_sub');
+        } else if (gifRect.left >= krace_minX && gifRect.right <= krace_maxX && gifRect.top >= krace_minY && gifRect.bottom <= krace_maxY) {
             message.textContent = `경륜! 현재 좌표: (${gifRect.left.toFixed(2)}, ${gifRect.top.toFixed(2)})`;
             message.style.display = 'block';
-        }
-        else if(gifRect.left >= krun_minX && gifRect.right <= krun_maxX && gifRect.top >= krun_minY && gifRect.bottom <= krun_maxY) {
-            var message = document.getElementById('event_sub');
+        } else if (gifRect.left >= krun_minX && gifRect.right <= krun_maxX && gifRect.top >= krun_minY && gifRect.bottom <= krun_maxY) {
             message.textContent = `달리기! 현재 좌표: (${gifRect.left.toFixed(2)}, ${gifRect.top.toFixed(2)})`;
             message.style.display = 'block';
+        } else {
+            message.style.display = 'none';
+            return;
         }
+        
     }, 2100); // 600ms 후 검사
 
     
