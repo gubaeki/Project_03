5257fdbef9d482f78b6ceee6c7148777d02f7446
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-07-19 13:52:58 +0900

    Update event_menu.js

--- assets/js/event_menu.js
+++ assets/js/event_menu.js
@@ -2,7 +2,10 @@ document.addEventListener('click', function(event) {
     var moving_tiger = document.getElementById('moving_tiger');
     var x = event.clientX;
     var y = event.clientY;
+
+    var centerx = x - 58;
+    var centery = y - 65;
     
-    moving_tiger.style.left = x + 'px';
-    moving_tiger.style.top = y + 'px';
+    moving_tiger.style.left = centerx + 'px';
+    moving_tiger.style.top = centery + 'px';
 });
