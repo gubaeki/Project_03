85857b7a718050dd9492b9ebbcd958b91abc4839
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-07-20 22:06:58 +0900

    Add files via upload

--- assets/js/event_menu.js
+++ assets/js/event_menu.js
@@ -1,20 +1,35 @@
+    // banner2 크기 정의
+    var banner2 = document.getElementById('banner2');
+    var banner2Rect = banner2.getBoundingClientRect();
+    var banner2Width = banner2Rect.right - banner2Rect.left;
+    var banner2Height = banner2Rect.bottom - banner2Rect.top;
+    var innerbanner2Width = Math.round(banner2Height / 2.1636);
+    var centerbanner2X = banner2Width / 2;
+    var minbanner2X = centerbanner2X - (innerbanner2Width / 2); 
+    var maxbanner2X = centerbanner2X + (innerbanner2Width / 2);
+
+
+    // event 종류 관련 변수
+    var select_event = 0;
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var message = document.getElementById('event_sub');
+    var moving_click1 = document.getElementById('moving_click1');
+    var moving_click2 = document.getElementById('moving_click2');
+    var moving_click3 = document.getElementById('moving_click3');
     var moving_tiger = document.getElementById('moving_tiger');
     var highlightKboat = document.getElementById('highlight_kboat');
     var highlightKrace = document.getElementById('highlight_krace');
     var highlightKrun = document.getElementById('highlight_krun');
 
-    // 경정, 경륜, 달리기 범위 표시
-    var viewportHeight = window.innerHeight;
-    var viewportWidth = window.innerWidth;
-
-    var kboat_minX_float = 52 * viewportWidth / 100, kboat_maxX_float = 96 * viewportWidth / 100;
-    var kboat_minY_float = 23 * viewportHeight / 100, kboat_maxY_float = 35.5 * viewportHeight / 100;
-    var krace_minX_float = 10 * viewportWidth / 100, krace_maxX_float = 37.3 * viewportWidth / 100;
-    var krace_minY_float = 45 * viewportHeight / 100, krace_maxY_float = 62.7 * viewportHeight / 100;
-    var krun_minX_float = 61.6 * viewportWidth / 100, krun_maxX_float = 96 * viewportWidth / 100;
-    var krun_minY_float = 75.9 * viewportHeight / 100, krun_maxY_float = 89 * viewportHeight / 100;
+    // 선택구역 나누기
+    var kboat_minX_float = centerbanner2X + (innerbanner2Width * 10 /100), kboat_maxX_float = centerbanner2X + (innerbanner2Width * 47 /100);
+    var kboat_minY_float = 30 * banner2Height / 100, kboat_maxY_float = 47 * banner2Height / 100;
+    var krace_minX_float = centerbanner2X - (innerbanner2Width * 47 /100), krace_maxX_float = centerbanner2X - (innerbanner2Width * 5 /100);
+    var krace_minY_float = 55 * banner2Height / 100, krace_maxY_float = 72 * banner2Height / 100;
+    var krun_minX_float = centerbanner2X + (innerbanner2Width * 11 /100), krun_maxX_float = centerbanner2X + (innerbanner2Width * 47 /100);
+    var krun_minY_float = 75 * banner2Height / 100, krun_maxY_float = 92 * banner2Height / 100;
 
     var kboat_minX = Math.round(kboat_minX_float), kboat_maxX = Math.round(kboat_maxX_float);
     var kboat_minY = Math.round(kboat_minY_float), kboat_maxY = Math.round(kboat_maxY_float);
@@ -23,6 +38,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var krun_minX = Math.round(krun_minX_float), krun_maxX = Math.round(krun_maxX_float);
     var krun_minY = Math.round(krun_minY_float), krun_maxY = Math.round(krun_maxY_float);
 
+    // 선택구역 하이라이트(평소엔 비활성화)
+    /*
     highlightKboat.style.left = kboat_minX + 'px';
     highlightKboat.style.top = kboat_minY + 'px';
     highlightKboat.style.width = (kboat_maxX - kboat_minX) + 'px';
@@ -37,10 +54,29 @@ document.addEventListener('DOMContentLoaded', function() {
     highlightKrun.style.top = krun_minY + 'px';
     highlightKrun.style.width = (krun_maxX - krun_minX) + 'px';
     highlightKrun.style.height = (krun_maxY - krun_minY) + 'px';
+    */
 
-    
+    // Click.gif 위치 정의
+    moving_click1.style.left = kboat_minX + (innerbanner2Width * 5 /100) + 'px';
+    moving_click1.style.top = kboat_maxY - (banner2Height * 4 /100) + 'px';
+    moving_click2.style.left = krace_minX + (innerbanner2Width * 5 /100)+ 'px';
+    moving_click2.style.top = krace_maxY - (banner2Height * 4 /100)+ 'px';
+    moving_click3.style.left = krun_minX + (innerbanner2Width * 5 /100)+ 'px';
+    moving_click3.style.top = krun_maxY - (banner2Height * 4 /100)+ 'px';
+
     // 예/아니오 버튼 이벤트 핸들러 설정
     document.querySelector('#yes-btn').addEventListener('click', function(event) {
+        if(select_event===1){
+            console.log('경정선택');
+            location.href='kboat.html';
+        }else if(select_event===2){
+            console.log('경륜선택');
+            location.href='krace.html';
+        }
+        else{
+            console.log('달리기선택');
+            location.href='krun.html';
+        }
         message.style.display = 'none';
         event.stopPropagation(); // 이벤트 버블링을 막아 click 이벤트가 상위로 전달되지 않도록 함
     });
@@ -58,28 +94,31 @@ document.addEventListener('DOMContentLoaded', function() {
         var x = event.clientX;
         var y = event.clientY;
 
-        var centerx = x;
-        var centery = y;
-
-        moving_tiger.style.left = centerx + 'px';
-        moving_tiger.style.top = centery + 'px';
+        moving_tiger.style.left = x + 'px';
+        moving_tiger.style.top = y + 'px';
 
         setTimeout(function() {
             var gifRect = moving_tiger.getBoundingClientRect();
+            var gifRectCenterX = (gifRect.left + gifRect.right) / 2;
+            var gifRectCenterY = (gifRect.top + gifRect.bottom) / 2;
             var messageElement = document.getElementById('event_message');
 
-            if (gifRect.left >= kboat_minX && gifRect.left <= kboat_maxX && gifRect.top >= kboat_minY && gifRect.top <= kboat_maxY) {
-                messageElement.textContent = `Kboat(경정)에 도전하시겠습니까? 현재 좌표: (${gifRect.left}, ${gifRect.top})`;
+            if (gifRectCenterX >= kboat_minX && gifRectCenterX <= kboat_maxX && gifRectCenterY >= kboat_minY && gifRectCenterY <= kboat_maxY) {
+                messageElement.textContent = `Kboat(경정)에 도전하시겠습니까?`;
                 message.style.display = 'block';
-            } else if (gifRect.left >= krace_minX && gifRect.left <= krace_maxX && gifRect.top >= krace_minY && gifRect.top <= krace_maxY) {
-                messageElement.textContent = `Krace(경륜)에 도전하시겠습니까? 현재 좌표: (${gifRect.left}, ${gifRect.top})`;
+                select_event = 1;
+            } else if (gifRectCenterX >= krace_minX && gifRectCenterX <= krace_maxX && gifRectCenterY >= krace_minY && gifRectCenterY <= krace_maxY) {
+                messageElement.textContent = `Krace(경륜)에 도전하시겠습니까?`;
                 message.style.display = 'block';
-            } else if (gifRect.left >= krun_minX && gifRect.left <= krun_maxX && gifRect.top >= krun_minY && gifRect.top <= krun_maxY) {
-                messageElement.textContent = `국민체력100(달리기)에 도전하시겠습니까? 현재 좌표: (${gifRect.left}, ${gifRect.top})`;
+                select_event = 2;
+            } else if (gifRectCenterX >= krun_minX && gifRectCenterX <= krun_maxX && gifRectCenterY >= krun_minY && gifRectCenterY <= krun_maxY) {
+                messageElement.textContent = `국민체력100(달리기)에 도전하시겠습니까?`;
                 message.style.display = 'block';
+                select_event = 3;
             } else {
                 message.style.display = 'none';
             }
+
         }, 2100); // 2100ms 후 검사
     });
 });
