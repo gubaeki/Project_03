63daa68424cea566f8b8a1245ac9a62689dacb6e
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-07-26 09:19:53 +0900

    Update event_krun.js

--- assets/js/event_krun.js
+++ assets/js/event_krun.js
@@ -34,6 +34,26 @@ function isCollide(img1, img2) {
             let hurdle_centerX = rect_hurdle.left + (rect_hurdle.width / 2);
             let hurdle_centerY = rect_hurdle.top + (rect_hurdle.height / 2);
 
+            let inner_tiger_left = Math.round((tiger_centerX - 10));
+            let inner_tiger_right = Math.round((tiger_centerX + 10));
+            let inner_tiger_top = Math.round((tiger_centerY - 10));
+            let inner_tiger_bottom = Math.round(tiger_centerY + 10);
+
+            let inner_hurdle_left = Math.round((hurdle_centerX - 10));
+            let inner_hurdle_right = Math.round((hurdle_centerX + 10));
+            let inner_hurdle_top = Math.round((hurdle_centerY - 10));
+            let inner_hurdle_bottom = Math.round((hurdle_centerY + 10));
+
+            
+            /*//두번째 방식
+            let rect_tiger = img1.getBoundingClientRect();
+            let rect_hurdle = img2.getBoundingClientRect();
+
+            let tiger_centerX = rect_tiger.left + (rect_tiger.width / 2);
+            let tiger_centerY = rect_tiger.top + (rect_tiger.height / 2);
+            let hurdle_centerX = rect_hurdle.left + (rect_hurdle.width / 2);
+            let hurdle_centerY = rect_hurdle.top + (rect_hurdle.height / 2);
+
             let inner_tiger_left = Math.round((tiger_centerX - (rect_tiger.width * 0.4)));
             let inner_tiger_right = Math.round((tiger_centerX + (rect_tiger.width * 0.4)));
             let inner_tiger_top = Math.round((tiger_centerY - (rect_tiger.height * 0.1)));
@@ -43,7 +63,7 @@ function isCollide(img1, img2) {
             let inner_hurdle_right = Math.round((hurdle_centerX + (rect_hurdle.width * 0.4)));
             let inner_hurdle_top = Math.round((hurdle_centerY - (rect_hurdle.height * 0.3)));
             let inner_hurdle_bottom = Math.round((hurdle_centerY + (rect_hurdle.height * 1)));
-            
+            */
             /*// 기존 방식
             let rect_tiger = img1.getBoundingClientRect();
             let rect_hurdle = img2.getBoundingClientRect();
