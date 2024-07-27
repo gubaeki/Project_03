23b39a9795795f9a14752357d817d5ccd71e8955
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-07-28 00:07:51 +0900

    Add files via upload

--- assets/js/event_menu.js
+++ assets/js/event_menu.js
@@ -22,6 +22,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var highlightKboat = document.getElementById('highlight_kboat');
     var highlightKrace = document.getElementById('highlight_krace');
     var highlightKrun = document.getElementById('highlight_krun');
+    var highlightFencing = document.getElementById('highlight_fencing');
+    
 
     // 선택구역 나누기
     var kboat_minX_float = centerbanner2X - (innerbanner2Width * 44 /100), kboat_maxX_float = centerbanner2X - (innerbanner2Width * 5 /100);
@@ -30,6 +32,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var krace_minY_float = 27 * banner2Height / 100, krace_maxY_float = 44 * banner2Height / 100;
     var krun_minX_float = centerbanner2X - (innerbanner2Width * 41 /100), krun_maxX_float = centerbanner2X - (innerbanner2Width * 2 /100);
     var krun_minY_float = 11 * banner2Height / 100, krun_maxY_float = 28 * banner2Height / 100;
+    var fencing_minX_float = centerbanner2X + (innerbanner2Width * 6 /100), fencing_maxX_float = centerbanner2X + (innerbanner2Width * 43 /100);
+    var fencing_minY_float = 51 * banner2Height / 100, fencing_maxY_float = 68 * banner2Height / 100;
+
 
     var kboat_minX = Math.round(kboat_minX_float), kboat_maxX = Math.round(kboat_maxX_float);
     var kboat_minY = Math.round(kboat_minY_float), kboat_maxY = Math.round(kboat_maxY_float);
@@ -37,6 +42,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var krace_minY = Math.round(krace_minY_float), krace_maxY = Math.round(krace_maxY_float);
     var krun_minX = Math.round(krun_minX_float), krun_maxX = Math.round(krun_maxX_float);
     var krun_minY = Math.round(krun_minY_float), krun_maxY = Math.round(krun_maxY_float);
+    var fencing_minX = Math.round(fencing_minX_float), fencing_maxX = Math.round(fencing_maxX_float);
+    var fencing_minY = Math.round(fencing_minY_float), fencing_maxY = Math.round(fencing_maxY_float);
+
 
     // 선택구역 하이라이트(평소엔 비활성화)
     /*
@@ -54,6 +62,11 @@ document.addEventListener('DOMContentLoaded', function() {
     highlightKrun.style.top = krun_minY + 'px';
     highlightKrun.style.width = (krun_maxX - krun_minX) + 'px';
     highlightKrun.style.height = (krun_maxY - krun_minY) + 'px';
+
+    highlightFencing.style.left = fencing_minX + 'px';
+    highlightFencing.style.top = fencing_minY + 'px';
+    highlightFencing.style.width = (fencing_maxX - fencing_minX) + 'px';
+    highlightFencing.style.height = (fencing_maxY - fencing_minY) + 'px';
     */
 
     // Click.gif 위치 정의
@@ -70,15 +83,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // 예/아니오 버튼 이벤트 핸들러 설정
     document.querySelector('#yes-btn').addEventListener('click', function(event) {
         if(select_event===1){
-            console.log('경정선택');
             location.href='krun.html';
         }else if(select_event===2){
-            console.log('경륜선택');
+            location.href='krun.html';
+        }else if(select_event===3){
             location.href='krun.html';
         }
         else{
-            console.log('달리기선택');
-            location.href='krun.html';
+            location.href='fencing.html';
         }
         message.style.display = 'none';
         event.stopPropagation(); // 이벤트 버블링을 막아 click 이벤트가 상위로 전달되지 않도록 함
@@ -100,17 +112,21 @@ document.addEventListener('DOMContentLoaded', function() {
         var messageElement = document.getElementById('event_message');
 
         if (x >= kboat_minX && x <= kboat_maxX && y >= kboat_minY && y <= kboat_maxY) {
-            messageElement.textContent = `Kboat(경정)에 도전하시겠습니까?`;
+            messageElement.src ="images/message_krun.png";
             message.style.display = 'block';
             select_event = 1;
         } else if (x >= krace_minX && x <= krace_maxX && y >= krace_minY && y <= krace_maxY) {
-            messageElement.textContent = `Krace(경륜)에 도전하시겠습니까?`;
+            messageElement.src ="images/message_krun.png";
             message.style.display = 'block';
             select_event = 2;
         } else if (x >= krun_minX && x <= krun_maxX && y >= krun_minY && y <= krun_maxY) {
-            messageElement.textContent = `국민체력100(달리기)에 도전하시겠습니까?`;
+            messageElement.src ="images/message_krun.png";
             message.style.display = 'block';
             select_event = 3;
+        }else if (x >= fencing_minX && x <= fencing_maxX && y >= fencing_minY && y <= fencing_maxY) {
+            messageElement.src ="images/message_fencing.png";
+            message.style.display = 'block';
+            select_event = 4;
         } else {
             message.style.display = 'none';
         }
